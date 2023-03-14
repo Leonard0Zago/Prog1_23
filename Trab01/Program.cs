1ebb7f3768dd92351a8d2a4257d03f0f7cb10e47
Leonardo <110700602+Leonard0Zago@users.noreply.github.com>
2023-03-14 18:02:02 -0300

    Add files via upload

---
+++ Trab01/Program.cs
@@ -0,0 +1,217 @@
+﻿Console.WriteLine("Digite o valor a ser escrito por extenso:");
+string Valor = Console.ReadLine();
+int ValorInt = Convert.ToInt32(Valor);
+
+string Value = Convert.ToString(ValorInt);
+int qtdChars = Value.Length;
+
+if (ValorInt < 10)
+{
+    if (ValorInt == 1){
+        static string FormatReturningString(string stringToFormat) // Recebe uma referência
+    {
+    return stringToFormat.Replace("1" , "Um");// Substitui a primeira pela segunda
+    }
+    Console.WriteLine( FormatReturningString(Valor) );
+    }
+    if (ValorInt == 2){
+        static string FormatReturningString(string stringToFormat) // Recebe uma referência
+    {
+    return stringToFormat.Replace("2" , "Dois");// Substitui a primeira pela segunda
+    }
+    Console.WriteLine( FormatReturningString(Valor) );
+    }
+    if (ValorInt == 3){
+        static string FormatReturningString(string stringToFormat) // Recebe uma referência
+    {
+    return stringToFormat.Replace("3" , "Tres");// Substitui a primeira pela segunda
+    }
+    Console.WriteLine( FormatReturningString(Valor) );
+    }
+    if (ValorInt == 4){
+        static string FormatReturningString(string stringToFormat) // Recebe uma referência
+    {
+    return stringToFormat.Replace("4" , "Quatro");// Substitui a primeira pela segunda
+    }
+    Console.WriteLine( FormatReturningString(Valor) );
+    }
+    if (ValorInt == 5){
+        static string FormatReturningString(string stringToFormat) // Recebe uma referência
+    {
+    return stringToFormat.Replace("5" , "Cinco");// Substitui a primeira pela segunda
+    }
+    Console.WriteLine( FormatReturningString(Valor) );
+    }
+    if (ValorInt == 6){
+        static string FormatReturningString(string stringToFormat) // Recebe uma referência
+    {
+    return stringToFormat.Replace("6" , "Seis");// Substitui a primeira pela segunda
+    }
+    Console.WriteLine( FormatReturningString(Valor) );
+    }
+    if (ValorInt == 7){
+        static string FormatReturningString(string stringToFormat) // Recebe uma referência
+    {
+    return stringToFormat.Replace("7" , "Sete");// Substitui a primeira pela segunda
+    }
+    Console.WriteLine( FormatReturningString(Valor) );
+    }
+    if (ValorInt == 8){
+        static string FormatReturningString(string stringToFormat) // Recebe uma referência
+    {
+    return stringToFormat.Replace("8" , "Oito");// Substitui a primeira pela segunda
+    }
+    Console.WriteLine( FormatReturningString(Valor) );
+    }
+    if (ValorInt == 9){
+        static string FormatReturningString(string stringToFormat) // Recebe uma referência
+    {
+    return stringToFormat.Replace("9" , "Nove");// Substitui a primeira pela segunda
+    }
+    Console.WriteLine( FormatReturningString(Valor) );
+    }
+    return;
+}
+int Dez = 0;
+int ValorCem = ValorInt;
+if (ValorInt < 100)
+{
+    while (ValorCem > 10)
+    {
+    ValorCem = ValorCem - 10;
+    Dez++;
+    }
+
+    string Dezena = Convert.ToString(Dez);
+
+    if (Dez == 2){
+        static string FormatReturningString(string stringToFormat) // Recebe uma referência
+    {
+    return stringToFormat.Replace("2" , "Vinte e");// Substitui a primeira pela segunda
+    }
+    Console.WriteLine( FormatReturningString(Dezena) );
+    }
+    if (Dez == 3){
+        static string FormatReturningString(string stringToFormat) // Recebe uma referência
+    {
+    return stringToFormat.Replace("3" , "Trinta e");// Substitui a primeira pela segunda
+    }
+    Console.WriteLine( FormatReturningString(Dezena) );
+    }
+    if (Dez == 4){
+        static string FormatReturningString(string stringToFormat) // Recebe uma referência
+    {
+    return stringToFormat.Replace("4" , "Quarenta e");// Substitui a primeira pela segunda
+    }
+    Console.WriteLine( FormatReturningString(Dezena) );
+    }
+    if (Dez == 5){
+        static string FormatReturningString(string stringToFormat) // Recebe uma referência
+    {
+    return stringToFormat.Replace("5" , "Cinquenta e");// Substitui a primeira pela segunda
+    }
+    Console.WriteLine( FormatReturningString(Dezena) );
+    }
+    if (Dez == 6){
+        static string FormatReturningString(string stringToFormat) // Recebe uma referência
+    {
+    return stringToFormat.Replace("6" , "Sesenta e");// Substitui a primeira pela segunda
+    }
+    Console.WriteLine( FormatReturningString(Dezena) );
+    }
+    if (Dez == 7){
+        static string FormatReturningString(string stringToFormat) // Recebe uma referência
+    {
+    return stringToFormat.Replace("7" , "Setenta e");// Substitui a primeira pela segunda
+    }
+    Console.WriteLine( FormatReturningString(Dezena) );
+    }
+    if (Dez == 8){
+        static string FormatReturningString(string stringToFormat) // Recebe uma referência
+    {
+    return stringToFormat.Replace("8" , "Oitenta e");// Substitui a primeira pela segunda
+    }
+    Console.WriteLine( FormatReturningString(Dezena) );
+    }
+    if (Dez == 9){
+        static string FormatReturningString(string stringToFormat) // Recebe uma referência
+    {
+    return stringToFormat.Replace("9" , "Noventa e");// Substitui a primeira pela segunda
+    }
+    Console.WriteLine( FormatReturningString(Dezena) );
+    }
+
+    string Deze = Convert.ToString(ValorCem);
+
+if (ValorCem < 10)
+{
+    if (ValorCem == 1){
+        static string FormatReturningString(string stringToFormat) // Recebe uma referência
+    {
+    return stringToFormat.Replace("1" , "Um");// Substitui a primeira pela segunda
+    }
+    Console.WriteLine( FormatReturningString(Deze) );
+    }
+    if (ValorCem == 2){
+        static string FormatReturningString(string stringToFormat) // Recebe uma referência
+    {
+    return stringToFormat.Replace("2" , "Dois");// Substitui a primeira pela segunda
+    }
+    Console.WriteLine( FormatReturningString(Deze) );
+    }
+    if (ValorCem == 3){
+        static string FormatReturningString(string stringToFormat) // Recebe uma referência
+    {
+    return stringToFormat.Replace("3" , "Tres");// Substitui a primeira pela segunda
+    }
+    Console.WriteLine( FormatReturningString(Deze) );
+    }
+    if (ValorCem == 4){
+        static string FormatReturningString(string stringToFormat) // Recebe uma referência
+    {
+    return stringToFormat.Replace("4" , "Quatro");// Substitui a primeira pela segunda
+    }
+    Console.WriteLine( FormatReturningString(Deze) );
+    }
+    if (ValorCem == 5){
+        static string FormatReturningString(string stringToFormat) // Recebe uma referência
+    {
+    return stringToFormat.Replace("5" , "Cinco");// Substitui a primeira pela segunda
+    }
+    Console.WriteLine( FormatReturningString(Deze) );
+    }
+    if (ValorCem == 6){
+        static string FormatReturningString(string stringToFormat) // Recebe uma referência
+    {
+    return stringToFormat.Replace("6" , "Seis");// Substitui a primeira pela segunda
+    }
+    Console.WriteLine( FormatReturningString(Deze) );
+    }
+    if (ValorCem == 7){
+        static string FormatReturningString(string stringToFormat) // Recebe uma referência
+    {
+    return stringToFormat.Replace("7" , "Sete");// Substitui a primeira pela segunda
+    }
+    Console.WriteLine( FormatReturningString(Deze) );
+    }
+    if (ValorCem == 8){
+        static string FormatReturningString(string stringToFormat) // Recebe uma referência
+    {
+    return stringToFormat.Replace("8" , "Oito");// Substitui a primeira pela segunda
+    }
+    Console.WriteLine( FormatReturningString(Deze) );
+    }
+    if (ValorCem == 9){
+        static string FormatReturningString(string stringToFormat) // Recebe uma referência
+    {
+    return stringToFormat.Replace("9" , "Nove");// Substitui a primeira pela segunda
+    }
+    Console.WriteLine( FormatReturningString(Deze) );
+    }
+    return;
+}    
+
+
+
+
+}
