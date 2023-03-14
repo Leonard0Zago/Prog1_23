fa9e505686d00def382d1a07fc8fe23358b0100a
Leonardo Perosa Zago <408430@unoesc.lan>
2023-03-13 21:47:26 -0300

    aula string numeros e conversão

---
+++ Exe03/Program.cs
@@ -0,0 +1,19 @@
+﻿// Criar função estática
+static void FormatString(string stringToFormat) // Recebe um valor cria uma cópia da 'mensagem'
+    stringToFormat.Replace("Mundo" , "Marte");
+}
+
+static string FormatReturningString(string stringToFormat) // Recebe uma referência
+{
+    return stringToFormat.Replace("Mundo" , "Marte");// Substitui a primeira pela segunda
+}
+
+var mensagem = "Olá Mundo!";
+
+// Invocando o método sem retorno
+FormatString(mensagem);
+Console.WriteLine(mensagem);
+
+var outraMensagem = "Bom-dia Mundo!";
+Console.WriteLine( FormatReturningString(outraMensagem) );
+
